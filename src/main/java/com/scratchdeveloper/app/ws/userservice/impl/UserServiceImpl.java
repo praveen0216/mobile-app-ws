d63abd25dcdc09a534776b33279d8b741b23de61
praveen <praveen.kumar02@hotmail.com>
2021-03-06 03:38:33 +0530

    Constructor based dependency injection

--- src/main/java/com/scratchdeveloper/app/ws/userservice/impl/UserServiceImpl.java
+++ src/main/java/com/scratchdeveloper/app/ws/userservice/impl/UserServiceImpl.java
@@ -4,16 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.scratchdeveloper.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.scratchdeveloper.app.ws.ui.model.response.UserRest;
 import com.scratchdeveloper.app.ws.userservice.UserService;
+import com.scratchdeveloper.app.ws.utility.Utils;
 
 @Service
 public class UserServiceImpl implements UserService{
 
 	Map<String , UserRest> users; 
+	
+	Utils utils;
+	
+	public UserServiceImpl() {
+		
+	}
+	
+	@Autowired
+	public UserServiceImpl(Utils utils) {
+		this.utils=utils;
+		
+	}
 	
 	@Override
 	public UserRest createUser(UserDetailsRequestModel userDetails) {
@@ -22,7 +36,7 @@ public class UserServiceImpl implements UserService{
 		returnValue.setFirstName(userDetails.getFirstName());
 		returnValue.setLastName(userDetails.getLastName());
 		
-		String userId =UUID.randomUUID().toString();
+		String userId =utils.generateUserId();
 		returnValue.setUserId(userId);
 		
 		if(users == null) users=new HashMap<>();
